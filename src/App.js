72acf53e4f55a2d2bd8727a7fabd39d22dc8b547
lhsthree <lhsthree@gmail.com>
2020-10-15 01:41:41 -0500

    feat: disabled button and text area at appropiate times

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
-import React, { Component } from "react"
+import React, { useState, useEffect } from "react"
 
-class App extends Component {
-  render(){
+function App() {
+  const STARTING_TIME = 5
+
+  const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
+  const [wordCount, setWordCount] = useState(0)
+
+  function handleChange(e){
+    const {value} = e.target
+    setText(value)
+  }
+
+  function calculateWordCount(text){
+    const wordsArr = text.trim().split(' ')
+    const filteredWords = wordsArr.filter(word => word !== "")
+    return filteredWords.length
+  }
+
+  function startGame(){
+    setIsTimeRunning(true)
+    setTimeRemaining(STARTING_TIME)
+    setText("")
+  }
+
+  function endGame() {
+      setIsTimeRunning(false)
+      const numWords = calculateWordCount(text)
+      setWordCount(numWords)
+  }
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0){
+      setTimeout(() => {
+        setTimeRemaining(time => time - 1)
+      }, 1000)
+    }  else if (timeRemaining === 0) {
+      endGame()
+    }
+  }, [timeRemaining, isTimeRunning])
+
+
+  console.log(text)
     return (
       <div>
         <h1>Speed Typing Game</h1>
-        <textarea value="" />
-        <h4>Time reamaing:</h4>
-        <button>Press to Start</button>
-        <h1>Word Count:</h1>
+        <textarea 
+          disabled={!isTimeRunning}
+          onChange={handleChange}
+          value={text} />
+        <h4>Time reamaing: {timeRemaining}</h4>
+        <button disabled={isTimeRunning} onClick={startGame}>Press to Start</button>
+        <h1>Word Count: {wordCount} </h1>
       </div>
-      )
-  }
+    )
 }
 
 export default App
